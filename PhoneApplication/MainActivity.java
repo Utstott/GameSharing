82dd0db1b46ad407f7a6f963b69ef3d9602ac04e
Utstott <79802259+Utstott@users.noreply.github.com>
2021-03-28 11:55:34 +0300

    Create MainActivity.java
    Окно для входа в приложение, куда пользователь вводит свой логин и пароль

---
+++ PhoneApplication/MainActivity.java
@@ -0,0 +1,62 @@
+package com.example.loginscreen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Dialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    EditText mTextUsername;
+    EditText mTextPassword;
+    Button mButtonLogin;
+    TextView mTextViewRegister;
+    DatabaseHelper db;
+    ViewGroup progressView;
+    protected boolean isProgressShowing=false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        db = new DatabaseHelper(this);
+        mTextUsername = (EditText) findViewById(R.id.edittext_username);
+        mTextPassword = (EditText) findViewById(R.id.edittext_password);
+        mButtonLogin = (Button) findViewById(R.id.button_login);
+        mTextViewRegister = (TextView) findViewById(R.id.textview_register);
+        mTextViewRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent registerIntent = new Intent(MainActivity.this,RegisterActivity.class);
+                startActivity(registerIntent);
+            }
+        });
+
+        mButtonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String user = mTextUsername.getText().toString().trim();
+                String pwd = mTextPassword.getText().toString().trim();
+                Boolean res = db.checkUser(user,pwd);
+                if(res == true)
+                {
+                    Toast.makeText(MainActivity.this,"Login Success",Toast.LENGTH_SHORT).show();
+                    Intent shopIntent = new Intent(MainActivity.this,ShopActivity.class);
+                    startActivity(shopIntent);
+                }
+                else
+                {
+                    Toast.makeText(MainActivity.this,"Login Error",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+}
